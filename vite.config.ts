3660792be6dd3a44a2861831a4809dd3524afa3e
kiiruelvis4-star <kiiruelvis4@gmail.com>
2026-09-05 18:22:37 +0300

    Set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    base: process.env.VITE_BASE_URL || '/',
+    base: '/LITTLE-ROSES-/',
     plugins: [
       react(),
       tailwindcss(),
